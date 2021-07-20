0fcf31a4cd6fa30d6127a59378744cccb9798161
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-07-20 15:20:55 +0530

    get Element by set STL

--- KthSmallestLargestElement.cpp
+++ KthSmallestLargestElement.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 #include<algorithm>
+#include<iterator>
+#include<set>
+
 
 int getElementBySort(int arr[],int n,int k){
     std::sort(arr,arr+n);
     return arr[k-1];
 }
 
+int getElementBySet(int arr[],int n,int k){
+    std::set<int> s(arr,arr+n);
+    std::set<int>::iterator itr = s.begin();
+    std::advance(itr,k-1);
+    return *itr;
+}
 
 int main(){
     int n;
@@ -14,6 +23,6 @@ int main(){
     for(int i =0;i<n;i++){
         std::cin>>arr[i];
     }
-    std::cout<<getElementBySort(arr,n,3);
+    std::cout<<getElementBySet(arr,n,3);
     return 0;
 }
