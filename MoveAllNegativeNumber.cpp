2c322c5cdbcebce501339af2d26747697d1de588
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-07-22 15:02:18 +0530

    Union intersection of array

--- MoveAllNegativeNumber.cpp
+++ MoveAllNegativeNumber.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
-#include<algorithm>
+#include <algorithm>
 #include <iterator>
 using namespace std;
 
 void moveAllNegative(int arr[], int n)
 {
-    int j=0;
+    int j = 0;
     for (int i = 0; i < n; i++)
     {
-        
+
         if (arr[i] < 0)
         {
-           if (i != j){
-               swap(arr[i], arr[j]);
-               j++;
-           }
-                
-            
+            if (i != j)
+            {
+                swap(arr[i], arr[j]);
+                j++;
+            }
+        }
+    }
+}
+
+void moveAllNegativeTwoPointer(int arr[],int n){
+    int left=0,right=n-1;
+    while(left<=right){
+        if(arr[left]<0 && arr[right]<0){
+            left++;
+        }
+        else if(arr[left]>0 && arr[right] <0){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }else if(arr[left]>0 && arr[right]>0){
+            right--;
+
+        }else{
+            left++;
+            right--;
         }
     }
 }
@@ -30,9 +49,10 @@ int main()
     {
         cin >> arr[i];
     }
-    moveAllNegative(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    moveAllNegativeTwoPointer(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
